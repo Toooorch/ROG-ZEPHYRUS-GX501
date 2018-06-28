b4b12a5313bec8133ae99a54d43f767393d5b6f3
Torch <michal@michalsoukup.com>
2018-06-28 18:19:35 +0200

    mojave buld usb script

---
+++ Tools/makeDrive-Mojave.sh
@@ -0,0 +1,2 @@
+echo Make Install Medium on disk “Install”
+sudo /Applications/Install\ macOS\ Mojave\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/Install --applicationpath /Applications/Install\ macOS\ Mojave\ Beta.app —nointeraction
