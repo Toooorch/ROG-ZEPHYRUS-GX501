2ea78b8e2c14c3d12af779bd24768910a6730b7a
Torch <michal@michalsoukup.com>
2018-09-24 00:33:32 +0200

    removed beta from mojave create usb script

--- Tools/makeDrive-Mojave.sh
+++ Tools/makeDrive-Mojave.sh
@@ -1,2 +1,2 @@
 echo Make Install Medium on disk “Install”
-sudo /Applications/Install\ macOS\ Mojave\ Beta.app/Contents/Resources/createinstallmedia --volume /Volumes/Install
+sudo /Applications/Install\ macOS\ Mojave.app/Contents/Resources/createinstallmedia --volume /Volumes/Install
